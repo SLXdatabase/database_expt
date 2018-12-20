b6ca300cc1a94b111f28b5c7e52c863042ee7e09
mu001999 <mu001999@outlook.com>
2018-12-20 14:38:17 +0800

    初始化orm.py，设计接口以待实现，初步完成Model和Model元类设计

---
+++ orm.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+
+import MySQLdb as mdb
+
+
+class Field(object):
+    pass
+
+
+class Expr(object):
+    def __init__(self, model, kwargs):
+        pass
+
+    def update(self, **kwargs):
+        pass
+
+    def limit(self, rows, offset=None):
+        pass
+
+    def select(self):
+        pass
+
+    def count(self):
+        pass
+
+
+class ModelMetaclass(type):
+    table = None
+    fields = dict()
+
+    def __init__(self, name, bases, attrs):
+        super(ModelMetaclass, self).__init__(name, bases, attrs)
+        for key, val in self.__dict__.iteritems():
+            if isinstance(val, Field):
+                self.fields[key] = val
+
+
+class Model(object):
+    __metaclass__ = ModelMetaclass
+
+    def save(self):
+        pass
+
+    @classmethod
+    def where(cls, **kwargs):
+        return Expr(cls, kwargs)
+
+
+class Database(object):
+    autocommit = True
+    connection = None
+    db_config = dict()
+
+    @classmethod
+    def connect(cls, **db_config):
+        pass
+
+    @classmethod
+    def get_connection(cls):
+        pass
+
+    @classmethod
+    def execute(cls, *args):
+        pass
+
+    def __del__(self):
+        pass
+
+
+def execute_raw_sql(sql, params=None):
+    pass
