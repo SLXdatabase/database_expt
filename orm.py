c5cc88550638a117e0d1817630585540c276aef5
mu001999 <mu001999@outlook.com>
2018-12-20 16:27:53 +0800

    完成Database类

--- orm.py
+++ orm.py
@@ -52,7 +52,6 @@ class Model(object):
         return Expr(cls, kwargs)
 
 
-
 class Database(object):
     autocommit = True
     connection = None
@@ -60,18 +59,36 @@ class Database(object):
 
     @classmethod
     def connect(cls, **db_config):
-        pass
+        cls.connection = mdb.connect(
+            host=db_config.get('host', 'localhost'),
+            port=int(db_config.get('port', 2222)),
+            user=db_config.get('user', 'root'),
+            passwd=db_config.get('password', ''),
+            db=db_config.get('database', 'test'),
+            charset=db_config.get('charset', 'utf-8')
+        )
+        cls.connection.autocommit(cls.autocommit)
+        cls.db_config.update(db_config)
 
     @classmethod
     def get_connection(cls):
-        pass
+        if not cls.connection or not cls.connection.open:
+            cls.connect(**cls.db_config)
+        try:
+            cls.connection.ping()
+        except mdb.OperationalError:
+            cls.connect(**cls.db_config)
+        return cls.connection
 
     @classmethod
     def execute(cls, *args):
-        pass
+        cursor = cls.get_connection().cursor()
+        cursor.execute(*args)
+        return cursor
 
     def __del__(self):
-        pass
+        if self.connection and self.conn.open:
+            self.connection.close()
 
 
 def execute_raw_sql(sql, params=None):
