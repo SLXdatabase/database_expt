456b950edcf056add0948c790fe727ecbbb67503
strawberryluo <13998191106@126.com>
2018-12-24 10:23:36 +0800

    根据具体表结构完成相关表Model类的设计

--- model.py
+++ model.py
@@ -16,10 +16,45 @@ Database.connect(
 class UserModel(Model):
     table = 'User'
 
+    userid = Field()
+    username = Field()
+    password = Field()
+    record = Field()
+    permission = Field()
+
 
 class ScoreModel(Model):
     table = 'Score'
 
+    userid = Field()
+    movieid = Field()
+    comDate = Field()
+    value = Field()
+    content = Field()
+    watched = Field()
+    star = Field()
+
 
 class MovieModel(Model):
     table = 'Movie'
+
+    movieid = Field()
+    mname = Field()
+    director = Field()
+    editor = Field()
+    actor = Field()
+    mtype = Field()
+    countryOrLocation = Field()
+    language = Field()
+    date = Field()
+    duration = Field()
+    othername = Field()
+    IMDb = Field()
+    introduction = Field()
+    allActors = Field()
+    numOfEvaluator = Field()
+    star5 = Field()
+    star4 = Field()
+    star3 = Field()
+    star2 = Field()
+    star1 = Field()
