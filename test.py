8f9f0b446cf1c0fbfb7730814644976772e6143e
strawberryluo <13998191106@126.com>
2018-12-25 13:26:27 +0800

    设计测试函数，函数体待完成

---
+++ test.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+
+from orm import execute_raw_sql
+from model import UserModel, ScoreModel, MovieModel
+import unittest
+
+
+class TestUserModel(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_instance(self):
+        pass
+
+    def test_select(self):
+        pass
+
+    def test_update(self):
+        pass
+
+    def test_limit(self):
+        pass
+
+    def test_count(self):
+        pass
+
+
+class TestScoreModel(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_instance(self):
+        pass
+
+    def test_select(self):
+        pass
+
+    def test_update(self):
+        pass
+
+    def test_limit(self):
+        pass
+
+    def test_count(self):
+        pass
+
+
+class TestMovieModel(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_instance(self):
+        pass
+
+    def test_select(self):
+        pass
+
+    def test_update(self):
+        pass
+
+    def test_limit(self):
+        pass
+
+    def test_count(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
