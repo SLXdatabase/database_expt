29f9810af494cce1336eaee7c269b14ba19cd959
strawberryluo <13998191106@126.com>
2019-01-03 10:38:27 +0800

    更新模型model.py，映射最新各表

--- model.py
+++ model.py
@@ -58,3 +58,67 @@ class MovieModel(Model):
     star3 = Field()
     star2 = Field()
     star1 = Field()
+
+
+class DirectorModel(Model):
+    table = 'Director'
+
+    dirtorid = Field()
+    dirorname = Field()
+    dirsex = Field()
+    dirage = Field()
+    dircountry = Field()
+    dirheight = Field()
+    dirbirthplace = Field()
+    dirnation = Field()
+    dirconstellation = Field()
+
+
+class EditorModel(Model):
+    table = 'Editor'
+
+    ediid = Field()
+    ediname = Field()
+    edisex = Field()
+    ediage = Field()
+    edicountry = Field()
+    ediheight = Field()
+    edibirthplace = Field()
+    edination = Field()
+    ediconstellation = Field()
+
+
+class ActorModel(Model):
+    table = 'Actor'
+
+    actorid = Field()
+    actorname = Field()
+    sex = Field()
+    age = Field()
+    country = Field()
+    height = Field()
+    birthplace = Field()
+    nation = Field()
+    constellation = Field()
+
+
+class MovActModel(Model):
+    table = 'MovAct'
+
+    movieid = Field()
+    actorid = Field()
+    character = Field()
+
+
+class MDModel(Model):
+    table = 'MD'
+
+    movieid = Field()
+    dirtorid = Field()
+
+
+class MEModel(Model):
+    table = 'ME'
+
+    movieid = Field()
+    ediid = Field()
